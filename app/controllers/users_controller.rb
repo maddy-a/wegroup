bc0cc8398c18a9715cb1ded84a9b62c8063ff21b
Jiajun <Jiajun@server>
2012-07-20 04:34:44 -0600

    Finish Home page and potential links to other pages; Finish initial styling using Bootstrap; also add form of session in home page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  def new
+  end
+  
+  def create
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  def index
+  end
+end
