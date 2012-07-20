30a364cfd20401139e5ffa6edadf2e5dfb0d6773
Jiajun <Jiajun@server>
2012-07-20 01:04:10 -0600

    Initialization including README and switching to PG db are done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wegroup::Application.config.secret_token = 'df9ef9ffcd1ea44c69674ee3dd517ccadba6b50b7d22225042a8cc1374fc02ca30730fc73c2936de64d0877b6fa94dfe46d275ea9cdc2ec50f4049b8504140c0'
